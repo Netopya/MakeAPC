d2d093dc81ef697548c616db34b8e35a8aa33124
Michael B <netopya@netopyaplanet.com>
2017-05-08 17:30:49 -0400

    #2 Fixed typo

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 
-app.use(express.static(__dirname + '/images'));
+app.use(app.static(__dirname + '/images'));
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
