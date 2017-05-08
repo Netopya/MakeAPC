ec6d76808180e131f46c338fe4957e99eada8195
Michael B <netopya@netopyaplanet.com>
2017-05-08 17:12:43 -0400

    #2 Modified listening port

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ app.get('/', function(req, res){
   res.send('<h1>Hello world</h1>');
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(8080, function(){
+  console.log('listening on *:8080');
 });
 
 console.log("Hello World!");
