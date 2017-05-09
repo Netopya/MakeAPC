f63e80130df1d7ff6cc96134ca0b704ba6f03b6c
Michael B <netopya@netopyaplanet.com>
2017-05-09 14:57:02 -0400

    #3 Added initial movement logic

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/users/', function(req, res){
+  res.json(io.sockets.sockets);
+});
+
 http.listen(8080, function(){
   console.log('listening on *:8080');
 });
@@ -25,6 +29,18 @@ io.on('connection', function (socket) {
       id: socket.id,
       username: socket.username,
       character: socket.character
+    });
+  });
+
+  socket.on('move', function(data){
+    
+    socket.x = data.x;
+    socket.y = data.y;
+
+    socket.broadcast.emit('user moved', {
+      id: socket.id,
+      x: socket.x,
+      y: socket.y
     });
   });
 
